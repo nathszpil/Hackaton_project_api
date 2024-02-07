d9ef4da7b277e957daa48c1436d1b9eb6a2d2057
nathszpil <nathanszpilman@hotmail.com>
2024-02-07 20:59:37 +0200

    updated files

--- inference_simple.py
+++ inference_simple.py
@@ -44,4 +44,4 @@ def add_one():
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8080)  # Run on localhost, port 8080
+    app.run(host='0.0.0.0', port=8080)  # Run on localhost, port 8080
